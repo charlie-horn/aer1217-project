f1f36d0d973519d0984ce6ce8f77ae53f23ce4cd
charlie-horn <charlie.horn@queensu.ca>
2021-04-08 22:13:35 -0700

    reverted to single project

--- extras/build/ardrone_autonomy/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/charlie/aer1217/extras/src/ardrone_autonomy
-# Build directory: /home/charlie/aer1217/extras/build/ardrone_autonomy
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

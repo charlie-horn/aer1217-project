94f8e3cf98172bb02c49150c18ce0dffe220caad
charlie-horn <charlie.horn@queensu.ca>
2021-04-15 07:49:07 -0700

    fix typo

--- refresh_source.sh
+++ refresh_source.sh
@@ -1,3 +1,3 @@
 cp -r src/aer1217_ardrone_simulator/ ~/aer1217/labs/src/
-cp ./ardrone_simulator.launch ~/aer1217/labs/src/aer1217_ardron_simulator/launch
+cp ./ardrone_simulator.launch ~/aer1217/labs/src/aer1217_ardrone_simulator/launch
 cp ./project2021.world ~/aer1217/gazebo/world/project2021.world
